c17b674f780b4e0c5a4d6938d8bab48f0903a820
ClinkzM <2148839177@qq.com>
2018-07-27 03:11:08 +0800

    use codemirror

--- blogs/blogID/blog.js
+++ blogs/blogID/blog.js
@@ -1,10 +1,12 @@
+var log = console.log.bind(console)
+
 var ajax = (request) => {
     var r = new XMLHttpRequest()
     r.open(request.method, request.url, true)
     if (request.contentType !== undefined) {
         r.setRequestHeader('Content-Type', request.contentType)
     }
-    r.onreadystatechange = function() {
+    r.onreadystatechange = () => {
         if (r.readyState === 4) {
             request.callback(r.response)
         }
@@ -25,12 +27,29 @@ var cleanData = (data, dataID) => {
                 author: "风行",
                 time: new Date(data[i].created_at).toLocaleString(),
                 updateTime: new Date(data[i].updated_at).toLocaleString(),
-                content: markdownCompile(data[i].body),
+                content: data[i].body,
             }
             return blog
         }
     }
 }
+
+// var templateNav = () => {
+//     var t = `
+//         <nav id="id-nav-box" class="content-box fengxing-clear">
+//             <div class="menu-item"><a href="/">主页</a></div>
+//             <div class="menu-item"><a href="/blogs/build/index.html">博客</a></div>
+//             <div class="menu-item"><a href="/apps/apps.html">应用</a></div>
+//         </nav>
+//     `
+//     return t
+// }
+//
+// var insertNav = () => {
+//     var html = templateNav()
+//     var div = document.querySelector('.blog-container')
+//     appendHtml(div, html)
+// }
 
 var templateBlog = (blog) => {
     var t = `
@@ -42,18 +61,73 @@ var templateBlog = (blog) => {
                 &nbsp;最后更新于<time>${blog.updateTime}</time>
             </div>
             <div class="blog-content">
-                ${blog.content}
+                ${templateContent(blog)}
             </div>
-            <a href="/blogs/build/index.html">返回</a>
         </div>
     `
     return t
+}
+
+var divideContent = (string) => {
+    var arr = string.split('```')
+    return arr
 }
 
 var markdownCompile = (text) => {
     var converter = new showdown.Converter()
     var md = converter.makeHtml(text)
     return md
+}
+
+var usingCodeMirror = (blog) => {
+    var textArea = document.querySelectorAll('textarea')
+    for (var i = 0; i < textArea.length; i++) {
+        var editor = CodeMirror.fromTextArea(textArea[i], {
+            // 显示行号
+            lineNumbers: true,
+            // 缩进单位为4
+            indentUnit: 4,
+            // 当前行背景高亮
+            styleActiveLine: true,
+            // 括号匹配
+            matchBrackets: true,
+            // HMTL混合模式
+            mode: 'text/htmlmixed',
+            mode: "text/javascript",
+            mode: "text/css",
+            // 自动换行
+            lineWrapping: true,
+             // 编辑器主题
+            theme: 'dracula',
+            foldGutter: true,
+            gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"],
+            //括号匹配
+            matchBrackets: true,
+            //只读
+            readOnly: true,
+        })
+    }
+}
+
+var templateContent = (blog) => {
+    var allString = blog.content
+    var a = divideContent(allString)
+    var html = ''
+    for (var i = 0; i < a.length; i++) {
+        var includesCode = a[i].includes('Html') || a[i].includes('JavaScript') || a[i].includes('Css')
+        var capital = a[i][0] === 'H' || a[i][0] === 'J' || a[i][0] === 'C'
+        if (includesCode && capital) {
+            var t = `
+            <textarea class="form-control" name="code">
+                ${a[i]}
+            </textarea>
+            `
+        } else {
+            var t = markdownCompile(a[i])
+        }
+        html = html + t
+    }
+    return html
 }
 
 var insertBlog = (blog) => {
@@ -66,6 +140,7 @@ var changeTitle = (blog) => {
     var t = document.querySelector('title')
     t.innerHTML = `${blog.author}@${blog.title}`
 }
+
 var handleDataID = () => {
     var blog = document.querySelector('.fengxing-blog')
     var id = Number(blog.dataset.id)
@@ -84,12 +159,14 @@ var handleData = () => {
             var blog = cleanData(data, dataID)
             insertBlog(blog)
             changeTitle(blog)
+            usingCodeMirror(blog)
         },
     }
     ajax(request)
 }
 
 var __main = () => {
+    // insertNav()
     handleData()
 }
 __main()

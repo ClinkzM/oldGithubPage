57c78b97e13dfbebc3f0681c6eb19199c1c07279
ClinkzM <2148839177@qq.com>
2019-04-13 22:42:39 +0800

    修复获取博客数据的bug

--- blogs/src/main/content.js
+++ blogs/src/main/content.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react'
 import './content.css'
 import BlogList from './bloglist'
 
-var ajax = (request) => {
-    var r = new XMLHttpRequest()
+const ajax = (request) => {
+    const r = new XMLHttpRequest()
     r.open(request.method, request.url, true)
     if (request.contentType !== undefined) {
         r.setRequestHeader('Content-Type', request.contentType)
@@ -33,37 +33,42 @@ class Content extends Component {
     }
 
     cleanData = (data) => {
-        var blogs = []
-        for (var i = 0; i < data.length; i++) {
-            if (data[i].labels[0].name === "blog") {
-                var blog = {
-                    id: i,
-                    dataID: data[i].id,
-                    title: data[i].title,
-                    author: "@风行",
-                    time: new Date(data[i].created_at).toLocaleString(),
-                    content: data[i].body,
-                    link: "阅读全文",
+        const blogs = []
+        for (let i = 0; i < data.length; i++) {
+            // console.log('data', data)
+            const label = data[i].labels
+            // console.log('label', label)
+            if (label.length !== 0) {
+                const labelName = data[i].labels[0].name
+                if (labelName && labelName=== "oldBlog") {
+                    const blog = {
+                        id: i,
+                        dataID: data[i].id,
+                        title: data[i].title,
+                        author: "@Clinkz",
+                        time: new Date(data[i].created_at).toLocaleString(),
+                        content: data[i].body,
+                        link: "阅读全文",
+                    }
+                    blogs.push(blog)
                 }
-                blogs.push(blog)
-            } else {
-                continue
             }
+
         }
         return blogs
     }
 
     fetchData = () => {
         const _this = this
-        var request = {
+        const request = {
             method: 'GET',
             url: 'https://api.github.com/repos/ClinkzM/blogs/issues',
             contentType: 'application/json',
             callback: function(response) {
                 // 不考虑错误情况（断网、服务器返回错误等等）
-                var data = JSON.parse(response)
-                var bs = _this.cleanData(data)
-                var state = {
+                const data = JSON.parse(response)
+                const bs = _this.cleanData(data)
+                const state = {
                     blogs: bs,
                 }
                 _this.setState(state)
